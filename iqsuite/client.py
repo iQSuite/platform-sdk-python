68678fb5ed430d1e948f2b5bdeff6135ec40f0de
Abhishek Choudhury <abhishek.choudhury@bluehexsoftware.com>
2025-01-07 13:59:40 +0530

    base url

--- iqsuite/client.py
+++ iqsuite/client.py
@@ -18,7 +18,7 @@ class IQSuiteClient:
     def __init__(
         self,
         api_key: str,
-        base_url: str = "https://iqsuite.cooliocorp.com/api/v1",
+        base_url: str = "https://staging.iqsuite.ai/api/v1",
         verify_ssl: bool = True,
         verbose: bool = False,
     ):
